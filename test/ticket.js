6194f898a6a324f95da0630ec9f73df2d82ecf32
home <home@homes-MBP.attlocal.net>
2021-06-06 13:14:06 -0700

    jam sometests

--- test/ticket.js
+++ test/ticket.js
@@ -2,7 +2,9 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 const testURI = "ipfs://SOME_HASH_SHIT";
+const testBoopURI = "testBoop";
 const FIRST_TICKET = 1;
+const SECOND_TICKET = 2;
 
 let Ticket;
 let ticket;
@@ -11,7 +13,7 @@ let notowner;
 describe("Ticket shit", function () {
   beforeEach(async function () {
     Ticket = await ethers.getContractFactory("TicketMaster");
-    ticket = await Ticket.deploy(100, testURI);
+    ticket = await Ticket.deploy(1);
     [owner, notowner] = await ethers.getSigners();
 
     await ticket.deployed();
@@ -32,6 +34,11 @@ describe("Ticket shit", function () {
     expect(ownerOfFirstTicket).to.equal(owner.address);
   });
 
+  it("Should assign the total supply of tickets to the owner", async function() {
+    const ownerBalance = await ticket.balanceOf(owner.address)
+    expect(await ticket.totalSupply()).to.equal(ownerBalance)
+  });
+
   it("Should have the uri we set", async function () {
     await ticket.createTicket(owner.address, testURI);
     const uri = await ticket.tokenURI(FIRST_TICKET);
@@ -43,5 +50,36 @@ describe("Ticket shit", function () {
     await expect(
       wrongTicketSigner.createTicket(owner.address, testURI)
     ).to.be.revertedWith("Ownable: caller is not the owner");
+  });
+
+  it("Should transfer between accounts", async function() {
+    await ticket.transferFrom(owner.address, notowner.address, FIRST_TICKET);
+    const newOwner = await ticket.ownerOf(FIRST_TICKET)
+    expect(newOwner).to.not.equal(owner.address)
+    expect(newOwner).to.equal(notowner.address)
+  });
+
+  it("Should fail if not enough tickets in supply", async function() {
+    await ticket.transferFrom(owner.address, notowner.address, FIRST_TICKET);
+    await expect(
+      ticket.transferFrom(owner.address, notowner.address, SECOND_TICKET)
+    ).to.be.revertedWith("nonexistent token")
+    const ownerBalance = await ticket.balanceOf(owner.address);
+    expect(ownerBalance).to.equal(0);
+  });
+
+  it("Should update supply after transfers", async function() {
+    await ticket.transferFrom(owner.address, notowner.address, FIRST_TICKET);
+    const ownerBalance = await ticket.balanceOf(owner.address);
+    const notOwnerBalance = await ticket.balanceOf(notowner.address);
+    expect(ownerBalance).to.equal(0);
+    expect(notOwnerBalance).to.equal(1);
+  });
+
+  it("Should be booped", async function() {
+    const uri = await ticket.tokenURI(FIRST_TICKET);
+    await ticket.boopTicket(FIRST_TICKET, testBoopURI);
+    const newURI = await ticket.tokenURI(FIRST_TICKET);
+    expect(newURI).to.not.equal(uri);
   });
 });
